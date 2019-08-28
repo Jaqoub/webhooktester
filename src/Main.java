3745d67f1ace37c2b5a2ddec1e4c2f5f8e7f0692
Jaqoub <33924194+Jaqoub@users.noreply.github.com>
2019-08-28 14:32:23 +0200

    Something something

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello 55500kkWorld!");
+        System.out.println("Hello 5550000kkWorld!");
     }
 }
